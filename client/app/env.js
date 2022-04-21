e3366fa809e4fbfe880d168c6605e00fc55173d1
EricMGustafson <emgustafson22@gmail.com>
2022-04-21 15:46:58 -0600

    hackathon!

---
+++ client/app/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-rsay4qjv.us.auth0.com'
+export const audience = 'https://EricDev.com'
+export const clientId = 't3yIT4nFRGtITax8lblMOK0oo6sn6H1F'
