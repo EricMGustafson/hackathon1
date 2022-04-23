1d0af449a13ebd7406e85df22c2d9fa67fa08618
EricMGustafson <emgustafson22@gmail.com>
2022-04-22 20:27:46 -0600

    tomfoolery

--- client/app/Services/CommentsService.js
+++ client/app/Services/CommentsService.js
@@ -3,6 +3,21 @@ import { Comment } from '../Models/Comment.js'
 import { ProxyState } from '../AppState.js'
 
 class CommentsService {
+  async createComment(formData) {
+    await api.post('api/comments', formData)
+    ProxyState.comments = [...ProxyState.comments, new Comment(formData)]
+  }
+
+  async editComment(formData) {
+    const commentId = formData.id
+    await api.put('api/comments/' + commentId, formData)
+    const index = ProxyState.comments.findIndex(c => c.id === commentId)
+    const editedComment = new Comment(formData)
+    ProxyState.comments.splice(index, 1, editedComment)
+    // eslint-disable-next-line no-self-assign
+    ProxyState.comments = ProxyState.comments
+  }
+
   async getAllComments() {
     const res = await api.get('api/comments')
     // eslint-disable-next-line no-console
